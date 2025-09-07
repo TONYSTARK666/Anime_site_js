316ebe7981d6e5f0e1c37c387e95d2e3240bb56e
Anastasia K. <nastya.kaaaa@mail.ru>
2025-09-07 22:34:38 +0300

    #Add Top-5 Anime

--- js/mainData.js
+++ js/mainData.js
@@ -1,10 +1,43 @@
 const mainData = () => {
+    const renderAnimeList = (array, genre) => {};
+
+    const renderTopAnime = (array) => {
+        const wrapper = document.querySelector(".filter__gallery");
+
+        wrapper.innerHTML = "";
+
+        array.forEach((item) => {
+            wrapper.insertAdjacentHTML(
+                "afterbegin",
+                `
+                    <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
+                        <div class="ep">18 / ${item.rating}</div>
+                        <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                        <h5><a href="/anime-details.html">${item.title}</a></h5>
+                    </div>
+                `
+            );
+        });
+
+        wrapper.querySelectorAll(".set-bg").forEach((element) => {
+            element.style.backgroundImage = `url(${element.dataset.setbg})`;
+        });
+    };
+
     fetch("https://anime-js-b2bf0-default-rtdb.europe-west1.firebasedatabase.app/anime.json")
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            console.log(data);
+            const genres = new Set();
+
+            renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
+
+            data.forEach((item) => {
+                genres.add(item.ganre);
+            });
+
+            renderAnimeList(data, genres);
         });
 };
 
